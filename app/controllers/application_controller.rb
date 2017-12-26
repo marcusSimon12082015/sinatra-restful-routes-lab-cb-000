d8a0e3520b7df09a2017bf1d68f4b433e516200b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-26 12:43:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
+    binding.pry
     erb :edit
   end
 
@@ -26,7 +27,6 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
-    binding.pry
     @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
